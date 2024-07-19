1b73488fd801c23cfdac8c7fa5ab459b7f93e6ce
Muhammad Irtiza <91867702+Irtiza751@users.noreply.github.com>
2024-07-19 13:01:14 +0500

    Create linkedlist.ts

---
+++ packages/rootbox/src/linkedlist.ts
@@ -0,0 +1,14 @@
+class Vertex<T> {
+  public data: T;
+  public next: Vertex<T> | null = null;
+
+  constructor(data: T) {
+    this.data = data;
+  }
+}
+
+class LinkedList {
+  /**
+   * @Todo implement linked list
+   */
+}

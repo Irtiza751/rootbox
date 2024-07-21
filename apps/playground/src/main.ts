7020eb4a53c18fdfde4dfd6c23b4c15bf1b1fd87
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2024-07-21 19:56:37 +0500

    Added the linkedlist

--- apps/playground/src/main.ts
+++ apps/playground/src/main.ts
@@ -1,8 +1,20 @@
 import "./style.css";
-import { Stack } from "../../../packages/rootbox/dist";
+import { Stack, LinkedList } from "rootbox";
 
 const stack = new Stack();
+const list = new LinkedList<number>();
 
 stack.push(10);
+stack.push(10);
+stack.push(10);
+stack.push(10);
 
-console.log(stack);
+// console.log(stack);
+
+list.append(10);
+list.append(20);
+list.append(30);
+list.prepend(9);
+list.prepend(8);
+
+console.log(list);
